d6aa2e2076a833e7834de48a48b01b594f681578
jaysharma420 <110590331+jaysharma420@users.noreply.github.com>
2022-12-11 13:18:09 +0530

    Update usercontroller.js

--- src/controller/usercontroller.js
+++ src/controller/usercontroller.js
@@ -1,5 +1,7 @@
 const usermodel = require("../model/usermodel")
 const jwt = require("jsonwebtoken")
+const bcrypt = require('bcrypt')
+
 
 let reg = /^[a-zA-Z_ ]{2,50}$/
 let pass = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,15}$/
@@ -16,6 +18,9 @@ const registration = async function (req, res) {
         if (!password) return res.status(400).send({ status: false, message: "please enter password for registration" })
         if (!pass.test(password)) return res.status(400).send({ status: false, message: "password must have one capital one small one numericand one special character [#?!@$%^&*-] and length between 8-15" })
 
+        data.password = await bcrypt.hash(password, 10)
+
+
         let savedata = await usermodel.create(data)
         return res.status(200).send({ data: savedata })
 
@@ -28,22 +33,24 @@ const userlogin = async function (req, res) {
         let name = req.body.name
         let password = req.body.password
 
-        if (Object.keys(data).length == 0) return res.status(400).send({ status: false, message: "please provide data for registration" })
+        if (Object.keys(req.body).length == 0) return res.status(400).send({ status: false, message: "please provide name and password for login" })
 
-        if (!name) return res.status(400).send({ status: false, message: "please enter name for registration" })
+        if (!name) return res.status(400).send({ status: false, message: "please enter name" })
         if (!reg.test(name)) return res.status(400).send({ status: false, message: "name can have only alphabets and space" })
 
-        if (!password) return res.status(400).send({ status: false, message: "please enter password for registration" })
+        if (!password) return res.status(400).send({ status: false, message: "please enter password for login" })
         if (!pass.test(password)) return res.status(400).send({ status: false, message: "password must have one capital one small one numericand one special character [#?!@$%^&*-] and length between 8-15" })
 
+        let data = await usermodel.findOne({name:name})
+        if (!data) {
+            return res.status(404).send({ status: false, message: "User not found with this name" })
+        }
+        let checkpassword = await bcrypt.compare(password, data.password);
+        if (!checkpassword) return res.status(400).send({ status: false, message: "login failed this password not matches with name" })
 
-        let userdata = await usermodel.findOne({ name: name, password: password })
+        let token = jwt.sign({ id:data._id }, "secretkey")
 
-        if (!userdata) return res.status(404).send({ status: false, message: "no user found with this name and passsword" })
-
-        let token = jwt.sign({ id: userdata._id }, "secretkey")
-
-        return res.status(200).send({ status: true, data: { id: userdata._id, token: token } })
+        return res.status(200).send({ status: true, data: { id:data._id, token: token } })
 
     } catch (err) {
         return res.status(500).send({ status: false, msg: err.message })
