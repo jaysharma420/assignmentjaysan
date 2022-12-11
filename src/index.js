77729e6421d62f55ba2b083013808e1078ea4bf1
jaysharma420 <110590331+jaysharma420@users.noreply.github.com>
2022-12-11 13:12:58 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,6 +13,12 @@ mongoose.connect("mongodb+srv://jay420:gRLzeLdOa6ENyasF@cluster0.dnkg3q6.mongodb
 
 app.use('/', route)
 
+app.use("/*", function (req, res) {
+    return res.status(400).send({
+        status: false, message: "Make Sure Your Endpoint is Correct !!!"
+    })
+})
+
 
 app.listen(process.env.PORT || 3000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
